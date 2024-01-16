ee6add5fc8fde37db535bdb8e5e20a70b8b2b20a
MariaKar1991 <mariaskaravazaki@gmail.com>
2024-01-16 11:15:27 +0000

    Adding footer (html, css, ts) content

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Component, Inject } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
+  scrollToTop() {
+    this.document.body.scrollTop = 0;
+    this.document.documentElement.scrollTop = 0;
+  }
 }
