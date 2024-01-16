5f82d6e82e7cb5b208481b03b355609678014da9
MariaKar1991 <mariaskaravazaki@gmail.com>
2024-01-16 13:29:36 +0000

    About/Contact pages routes and adding them in navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { ContactComponent } from './components/contact/contact.component';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { Error404Component } from './components/error-404/error-404.component';
@@ -5,15 +6,12 @@ import { StoreComponent } from './components/store/store.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { StoreProductsComponent } from './components/store/store-products/store-products.component';
 
-
-
 export const routes: Routes = [
-   
-    {path: "", redirectTo: "home", pathMatch: "full"},
-    {path: "home", component: HomeComponent},
-    {path: "about-us", component: AboutUsComponent},
-    {path: "store", component: StoreComponent},
-    {path: "storeProducts/:id", component: StoreProductsComponent},
-    {path: "**", component: Error404Component},
-   
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'store', component: StoreComponent },
+  { path: 'storeProducts/:id', component: StoreProductsComponent },
+  { path: '**', component: Error404Component },
 ];
--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,14 +1,19 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [CommonModule,RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.css'
+  styleUrl: './about-us.component.css',
 })
 export class AboutUsComponent {
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
+  scrollToTop() {
+    this.document.body.scrollTop = 0;
+    this.document.documentElement.scrollTop = 0;
+  }
 }
